a28ef6501ab212384b1af431ef19dc86052413d2
Gustavo Belo <gustavobelo28@gmail.com>
2021-06-03 12:01:01 -0300

    End of lecture 17 ;)

---
+++ PlayingCard/CMMotionManager+shared.swift
@@ -0,0 +1,13 @@
+//
+//  CMMotionManager+shared.swift
+//  PlayingCard
+//
+//  Created by Gustavo Belo on 02/06/21.
+//  Copyright © 2021 Stanford CS193p classes. All rights reserved.
+//
+
+import CoreMotion
+
+extension CMMotionManager {
+    static var shared = CMMotionManager()
+}
